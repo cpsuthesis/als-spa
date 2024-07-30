a1b5c71c7c43eb22e1e43f555e84664d1dba9fb1
Jhonmark P. Valdez <jmprudentevaldez@gmail.com>
2024-07-30 14:46:56 +0800

    fixed linting error

--- src/pages/auth/Login/page.tsx
+++ src/pages/auth/Login/page.tsx
@@ -11,9 +11,9 @@ export default function Login() {
         </div>
         <div>
           <p className="text-lg italic">
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
             labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-             nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in."
+             nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in.
           </p>
           <p className="text-md font-semibold mt-4">Alternative learning system</p>
         </div>
